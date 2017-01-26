f4267379f8b40c01e2670e7b5f46832fa3996501
puan0601 <anton.pugach@gmail.com>
2017-01-25 22:22:13 -0800

    finished shakespear

--- challenges/shakespearian-insult-generator.js
+++ challenges/shakespearian-insult-generator.js
@@ -16,4 +16,23 @@ var first_word = ["yeasty", "weedy", "wayward", "warped", "villainous", "venomed
 var second_word = ["weather-bitten", "unchin-snouted", "toad-spotted", "tickle-brained", "tardy-gaited", "swag-bellied", "spur-galled", "sheep-biting", "shard-borne", "rump-fed", "rude-growing", "rough-hewn", "reeling-ripe", "pox-marked", "pottle-deep", "plume-plucked", "onion-eyed", "motley-minded", "milk-livered", "knotty-pated", "ill-nurtured", "ill-breeding", "idle-headed", "hell-hated", "hedge-born", "hasty-witted", "half-faced", "guts-griping", "full-gorged", "fool-born", "folly-fallen", "fly-bitten", "flap-mouthed", "fen-sucked", "fat-kidneyed", "elf-skinned", "earth-vexing", "dread-bolted", "doghearted", "dizzy-eyed", "dismal-dreaming", "crook-pated", "common-kissing", "clay-brained", "clapper-clawed", "boil-brained", "beetle-headed", "beef-witted", "bat-fowling", "base-court"];
 var third_word = ["wagtail", "whey-face", "vassal", "varlet", "strumpet", "skainsmate", "scut", "ratsbane", "pumpion", "puttock", "pignut", "pigeon-egg", "nut-hook", "mumble-news", "moldwarp", "miscreant", "minnow", "measle", "mammet", "malt-worm", "maggot-pie", "lout", "lewdster", "joithead", "hugger-mugger", "horn-beast", "hedge-pig", "harpy", "haggard", "gudgeon", "giglet", "fustilarian", "foot-licker", "flirt-gill", "flax-wench", "flap-dragon", "dewberry", "death-token", "codpiece", "coxcomb", "clotpole", "clack-dish", "canker-blossom", "bum-bailey", "bugbear", "boar-pig", "bladder", "barnacle", "baggage", "apple-john"];
 
-// YOUR CODE HERE
+function generateInsults(num) {
+  var insults = []
+  while (num > 0) {
+    var randTwoMax = Math.floor(Math.random() * (3-0) + 0);
+    var randFiftyMax = Math.floor(Math.random() * (50-0) + 0); /* 50 = max, 0 = min*/
+    if (randTwoMax === 0) {
+      insults += third_word[randFiftyMax] + " ";
+    } else if (randTwoMax === 1) {
+      insults += second_word[randFiftyMax] + " " + third_word[randFiftyMax] + " ";
+    } else {
+      insults += first_word[randFiftyMax] + " " + second_word[randFiftyMax] + " " + third_word[randFiftyMax] + " ";
+    }
+    num--;
+    }
+  return insults;
+  }
+
+var name = prompt("Who would you like to libel today?");
+var num = prompt("How many insults do you want today?");
+alert(name + " is a " + generateInsults(num));
