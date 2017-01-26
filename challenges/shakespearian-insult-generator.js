008df2c02e189019b7204150daf37126ced182c7
chri-roch12 <chris.prochnow@gmail.com>
2017-01-25 16:50:22 -0800

    Basic function to generate insult from three arrays.

--- challenges/shakespearian-insult-generator.js
+++ challenges/shakespearian-insult-generator.js
@@ -17,3 +17,10 @@ var second_word = ["weather-bitten", "unchin-snouted", "toad-spotted", "tickle-b
 var third_word = ["wagtail", "whey-face", "vassal", "varlet", "strumpet", "skainsmate", "scut", "ratsbane", "pumpion", "puttock", "pignut", "pigeon-egg", "nut-hook", "mumble-news", "moldwarp", "miscreant", "minnow", "measle", "mammet", "malt-worm", "maggot-pie", "lout", "lewdster", "joithead", "hugger-mugger", "horn-beast", "hedge-pig", "harpy", "haggard", "gudgeon", "giglet", "fustilarian", "foot-licker", "flirt-gill", "flax-wench", "flap-dragon", "dewberry", "death-token", "codpiece", "coxcomb", "clotpole", "clack-dish", "canker-blossom", "bum-bailey", "bugbear", "boar-pig", "bladder", "barnacle", "baggage", "apple-john"];
 
 // YOUR CODE HERE
+
+var generateInsult = function() {
+  var generateWordFirst = first_word[Math.floor(Math.random() * first_word.length)];
+  var generateWordSecond = second_word[Math.floor(Math.random() * second_word.length)];
+  var generateWordThird = third_word[Math.floor(Math.random() * third_word.length)];
+    return (generateWordFirst + " " + generateWordSecond + " " + generateWordThird + "!")
+  };
