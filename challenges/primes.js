037a40bee9c3dbb0453c3ca0aee6a4aa02c58e64
rccacho <rccacho@gmail.com>
2017-01-25 22:59:17 -0800

    solved primes

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,3 +10,16 @@
 */
 
 // YOUR CODE HERE
+
+var isPrime = function(num) {
+  if (num <= 1) {
+  	return false;
+  } 
+  for (var i = 2; i < num; i++) {
+	if (num % i == 0) {
+      return false;
+	} else {
+	  return true;
+	}
+  }
+}
