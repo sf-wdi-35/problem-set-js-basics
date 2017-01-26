052b8f5ba6b05ba28ce0313621479b4813fa625c
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-01-26 15:16:47 -0800

    my solution

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,28 @@
 */
 
 // YOUR CODE HERE
+
+var bottles;
+
+for(i = 5; i >= 1; i--) {
+  if(i === 1) {
+    bottles = 'bottle';
+  } else {
+    bottles = 'bottles';
+  }
+
+  console.log( i + " " + bottles + " of beer on the wall.");
+
+  if(i < 5) {
+    console.log("");
+    console.log( i + " " + bottles + " of beer on the wall.");
+  }
+
+  console.log( i + " " + bottles + " of beer.");
+  console.log("Take one down.");
+  console.log("Pass it around.");
+
+  if (i === 1) {
+      console.log("No bottles of beer on the wall.");
+  }
+}
